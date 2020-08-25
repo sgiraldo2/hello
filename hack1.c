e33ccadf74f14fc1f26c878bdf503a5cb5e7ce48
Santiago Giraldo <69825324+sgiraldo2@users.noreply.github.com>
2020-08-24 21:34:52 -0500

    updated hello.c
    hack 1

---
+++ hack1.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Santiago Giraldo
+ * Hack 1
+ *This program prints out my name and major.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("I am Santiago Giraldo!\n");
+    printf("Majoring in Computer Science\n");
+
+    return 0;
+}
